18c7822e3325d4ca32e62d23bccb2d97bcbb990e
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-21 12:56:36 +0530

    indented code

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+//query
 import getAllBlogs from "../contentstack/queries/getAllBlogs";
+
+//function to get all blogs
 export default function Home(props) {
   let banner = props.banner;
-  console.log(banner);
   return (
     <>
       {props.banner.map((banner) => {
